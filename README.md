78d3a54d59d44ba53fa57aefd85b78da0dec9132
Quinn Johnson <winseven4lyf@gmail.com>
2019-02-15 12:59:58 +1000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,28 @@
+# `debounced-pin`
+
+[![Build Status](https://travis-ci.org/Winseven4lyf/rust-debounced-pin.svg)](https://travis-ci.org/Winseven4lyf/rust-debounced-pin)
+[![Crate](https://img.shields.io/crates/v/debounced-pin.svg)](https://crates.io/crates/debounced-pin)
+
+Adds a wrapper for an `InputPin` that debounces it's `is_high()` and `is_low()` methods.
+
+## Usage
+
+Add this to your `Cargo.toml`:
+
+```toml
+[dependencies]
+debounced-pin = "0.1.0"
+```
+
+This crate currently requires `embedded-hal` to be built using the `unproven` feature, for access to the `InputPin` trait.
+
+## License
+
+This project is licensed under either of
+
+- Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or
+  [online](https://www.apache.org/licenses/LICENSE-2.0))
+- MIT license ([LICENSE-MIT](LICENSE-MIT) or
+  [online](https://opensource.org/licenses/MIT))
+
+at your option.
---
+++ src/lib.rs
@@ -0,0 +1,109 @@
+//! Adds a wrapper for an `InputPin` that debounces it's `is_high()` and `is_low()` methods.
+
+#![cfg_attr(not(test), no_std)]
+
+use core::marker::PhantomData;
+use embedded_hal::digital::InputPin;
+
+/// Unit struct for active-low pins.
+pub struct ActiveLow;
+
+/// Unit struct for active-high pins.
+pub struct ActiveHigh;
+
+/// A debounced input pin.
+///
+/// Implements approach 1 from [here](http://www.labbookpages.co.uk/electronics/debounce.html#soft)
+/// ([archived 2018-09-03](https://web.archive.org/web/20180903142143/http://www.labbookpages.co.uk/electronics/debounce.html#soft)).
+///
+/// Requires [`update()`] to be called every ~1ms.
+///
+/// [`update()`]: #method.update
+pub struct DebouncedInputPin<T: InputPin, A> {
+    /// The wrapped pin.
+    pub pin: T,
+
+    /// Whether the pin is active-high or active-low.
+    activeness: PhantomData<A>,
+
+    /// The counter.
+    counter: i8,
+
+    /// The debounced state.
+    state: bool,
+}
+
+impl<T, A> DebouncedInputPin<T, A>
+where
+    T: InputPin,
+{
+    /// Initializes a new debounced input pin.
+    pub fn new(pin: T) -> Self {
+        DebouncedInputPin {
+            pin,
+            activeness: PhantomData,
+            counter: 0,
+            state: false,
+        }
+    }
+}
+
+impl<T, A> InputPin for DebouncedInputPin<T, A>
+where
+    T: InputPin,
+{
+    fn is_high(&self) -> bool {
+        self.state
+    }
+
+    fn is_low(&self) -> bool {
+        !self.state
+    }
+}
+
+impl<T> DebouncedInputPin<T, ActiveHigh>
+where
+    T: InputPin,
+{
+    /// Updates the debounce logic.
+    ///
+    /// Should be called every ~1ms.
+    pub fn update(&mut self) {
+        if self.pin.is_low() {
+            self.counter = 0;
+            self.state = false;
+        } else if self.counter < 10 {
+            self.counter += 1;
+        }
+
+        if self.counter == 10 {
+            self.state = true;
+        }
+    }
+}
+
+impl<T> DebouncedInputPin<T, ActiveLow>
+where
+    T: InputPin,
+{
+    /// Updates the debounce logic.
+    ///
+    /// Should be called every ~1ms.
+    pub fn update(&mut self) {
+        if self.pin.is_high() {
+            self.counter = 0;
+            self.state = false;
+        } else if self.counter < 10 {
+            self.counter += 1;
+        }
+
+        if self.counter == 10 {
+            self.state = true;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // TODO implement tests
+}
